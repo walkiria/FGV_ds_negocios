edcfe7abde969527f1f2503471012c4673402632
Walkiria Resende <walkiriaresende@gmail.com>
2022-10-31 17:06:49 -0300

    exemplo de OO e guia pra SQL

---
+++ aulas_praticas/SQL/banco_de_dados.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+
+INSERT INTO Persons (PersonID, LastName, FirstName, Address, City)
+VALUES (1, 'Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Norway');
